b3815aa2e42a4ff7f083bb58da611709598f9d8d
Ian Synge <gamersynge@gmail.com>
2025-06-12 11:14:21 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+require('datejs');
 
-
+function combineUsers(...users){
+  let combinedObject = {
+    users: []
+  };
+  users.forEach(score => combinedObject.users.push(...score));
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy")
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
